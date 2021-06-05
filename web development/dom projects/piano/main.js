22f3bc3e63d6d8924d802b4bd1804c5351257c36
harshith <harshithkanigarla784@gmail.com>
2021-06-05 10:35:57 +0530

    First commit

---
+++ web development/dom projects/piano/main.js
@@ -0,0 +1,327 @@
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 65) {
+      document.getElementById('audio24').play();
+      document.getElementById("a").style.backgroundColor = "#ccc";
+      document.getElementById("a").style.color = "#000";
+    }
+  });
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 66) {
+      document.getElementById('audio25').play();
+      document.getElementById("b").style.backgroundColor = "#ccc";
+      document.getElementById("b").style.color = "#000";
+    
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 67) {
+      document.getElementById('audio1').play();
+      document.getElementById("c").style.backgroundColor = "#ccc";
+      document.getElementById("c").style.color = "#000";
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 68) {
+      document.getElementById('audio2').play();
+      document.getElementById("d").style.backgroundColor = "#ccc";
+      document.getElementById("d").style.color = "#000";
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 69) {
+      document.getElementById('audio3').play();
+      document.getElementById("e").style.backgroundColor = "#ccc";
+      document.getElementById("e").style.color = "#000";
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 70) {
+      document.getElementById('audio4').play();
+      document.getElementById("f").style.backgroundColor = "#ccc";
+      document.getElementById("f").style.color = "#000";
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 71) {
+      document.getElementById('audio5').play();
+      document.getElementById("g").style.backgroundColor = "#ccc";
+      document.getElementById("g").style.color = "#000";
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 72) {
+      document.getElementById('audio6').play();
+      document.getElementById("h").style.backgroundColor = "#ccc";
+      document.getElementById("h").style.color = "#000";
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 73) {
+      document.getElementById('audio7').play();
+      document.getElementById("i").style.backgroundColor = "#ccc";
+      document.getElementById("i").style.color = "#000";
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 74) {
+      document.getElementById('audio8').play();
+      document.getElementById("j").style.backgroundColor = "#ccc";
+      document.getElementById("j").style.color = "#000";
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 75) {
+      document.getElementById('audio9').play();
+      document.getElementById("k").style.backgroundColor = "#ccc";
+      document.getElementById("k").style.color = "#000";
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 76) {
+      document.getElementById('audio10').play();
+      document.getElementById("l").style.backgroundColor = "#ccc";
+      document.getElementById("l").style.color = "#000";
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 77) {
+      document.getElementById('audio11').play();
+      document.getElementById("m").style.backgroundColor = "#ccc";
+      document.getElementById("m").style.color = "#000";
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 78) {
+      document.getElementById('audio13').play();
+      document.getElementById("n").style.backgroundColor = "#ccc";
+      document.getElementById("n").style.color = "#000";
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 79) {
+      document.getElementById('audio14').play();
+      document.getElementById("o").style.backgroundColor = "#ccc";
+      document.getElementById("o").style.color = "#000";
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 80) {
+      document.getElementById('audio15').play();
+      document.getElementById("p").style.backgroundColor = "#ccc";
+      document.getElementById("p").style.color = "#000";
+    }
+  });
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 81) {
+      document.getElementById('audio16').play();
+      document.getElementById("q").style.backgroundColor = "#ccc";
+      document.getElementById("q").style.color = "#000";
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 82) {
+      document.getElementById('audio17').play();
+      document.getElementById("r").style.backgroundColor = "#ccc";
+      document.getElementById("r").style.color = "#000";
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 83) {
+      document.getElementById('audio18').play();
+      document.getElementById("s").style.backgroundColor = "#ccc";
+      document.getElementById("s").style.color = "#000";
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 84) {
+      document.getElementById('audio19').play();
+      document.getElementById("t").style.backgroundColor = "#ccc";
+      document.getElementById("t").style.color = "#000";
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 85) {
+      document.getElementById('audio20').play();
+      document.getElementById("u").style.backgroundColor = "#ccc";
+      document.getElementById("u").style.color = "#000";
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 86) {
+      document.getElementById('audio21').play();
+      document.getElementById("v").style.backgroundColor = "#ccc";
+      document.getElementById("v").style.color = "#000";
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 87) {
+      document.getElementById('audio22').play();
+      document.getElementById("w").style.backgroundColor = "#ccc";
+      document.getElementById("w").style.color = "#000";
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 88) {
+      document.getElementById('audio23').play();
+      document.getElementById("x").style.backgroundColor = "#ccc";
+      document.getElementById("x").style.color = "#000";
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 89) {
+      document.getElementById('audio24').play();
+      document.getElementById("y").style.backgroundColor = "#ccc";
+      document.getElementById("y").style.color = "#000";
+    }
+  });document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 90) {
+      document.getElementById('audio25').play();
+      document.getElementById("z").style.backgroundColor = "#ccc";
+      document.getElementById("z").style.color = "#000";
+    }
+  });
+
+
+
+
+
+
+  document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 65) {
+      document.getElementById('audio24').play();
+      document.getElementById("a").style.backgroundColor = "cadetblue";
+      document.getElementById("a").style.color = "#fff";
+    }
+  });
+  document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 66) {
+      document.getElementById('audio25').play();
+      document.getElementById("b").style.backgroundColor = "cadetblue";
+      document.getElementById("b").style.color = "#fff";
+    
+    }
+  });document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 67) {
+      document.getElementById('audio1').play();
+      document.getElementById("c").style.backgroundColor = "cadetblue";
+      document.getElementById("c").style.color = "#fff";
+    }
+  });document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 68) {
+      document.getElementById('audio2').play();
+      document.getElementById("d").style.backgroundColor = "cadetblue";
+      document.getElementById("d").style.color = "#fff";
+    }
+  });document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 69) {
+      document.getElementById('audio3').play();
+      document.getElementById("e").style.backgroundColor = "cadetblue";
+      document.getElementById("e").style.color = "#fff";
+    }
+  });document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 70) {
+      document.getElementById('audio4').play();
+      document.getElementById("f").style.backgroundColor = "cadetblue";
+      document.getElementById("f").style.color = "#fff";
+    }
+  });document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 71) {
+      document.getElementById('audio5').play();
+      document.getElementById("g").style.backgroundColor = "cadetblue";
+      document.getElementById("g").style.color = "#fff";
+    }
+  });document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 72) {
+      document.getElementById('audio6').play();
+      document.getElementById("h").style.backgroundColor = "cadetblue";
+      document.getElementById("h").style.color = "#fff";
+    }
+  });document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 73) {
+      document.getElementById('audio7').play();
+      document.getElementById("i").style.backgroundColor = "cadetblue";
+      document.getElementById("i").style.color = "#fff";
+    }
+  });document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 74) {
+      document.getElementById('audio8').play();
+      document.getElementById("j").style.backgroundColor = "cadetblue";
+      document.getElementById("j").style.color = "#fff";
+    }
+  });document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 75) {
+      document.getElementById('audio9').play();
+      document.getElementById("k").style.backgroundColor = "cadetblue";
+      document.getElementById("k").style.color = "#fff";
+    }
+  });document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 76) {
+      document.getElementById('audio10').play();
+      document.getElementById("l").style.backgroundColor = "cadetblue";
+      document.getElementById("l").style.color = "#fff";
+    }
+  });document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 77) {
+      document.getElementById('audio11').play();
+      document.getElementById("m").style.backgroundColor = "cadetblue";
+      document.getElementById("m").style.color = "#fff";
+    }
+  })
+  document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 78) {
+      document.getElementById('audio13').play();
+      document.getElementById("n").style.backgroundColor = "cadetblue";
+      document.getElementById("n").style.color = "#fff";
+    }
+  });document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 79) {
+      document.getElementById('audio14').play();
+      document.getElementById("o").style.backgroundColor = "cadetblue";
+      document.getElementById("o").style.color = "#fff";
+    }
+  });document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 80) {
+      document.getElementById('audio15').play();
+      document.getElementById("p").style.backgroundColor = "cadetblue";
+      document.getElementById("p").style.color = "#fff";
+    }
+  });
+  document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 81) {
+      document.getElementById('audio16').play();
+      document.getElementById("q").style.backgroundColor = "cadetblue";
+      document.getElementById("q").style.color = "#fff";
+    }
+  });document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 82) {
+      document.getElementById('audio17').play();
+      document.getElementById("r").style.backgroundColor = "cadetblue";
+      document.getElementById("r").style.color = "#fff";
+    }
+  });document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 83) {
+      document.getElementById('audio18').play();
+      document.getElementById("s").style.backgroundColor = "cadetblue";
+      document.getElementById("s").style.color = "#fff";
+    }
+  });document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 84) {
+      document.getElementById('audio19').play();
+      document.getElementById("t").style.backgroundColor = "cadetblue";
+      document.getElementById("t").style.color = "#fff";
+    }
+  });document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 85) {
+      document.getElementById('audio20').play();
+      document.getElementById("u").style.backgroundColor = "cadetblue";
+      document.getElementById("u").style.color = "#fff";
+    }
+  });document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 86) {
+      document.getElementById('audio21').play();
+      document.getElementById("v").style.backgroundColor = "cadetblue";
+      document.getElementById("v").style.color = "#fff";
+    }
+  });document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 87) {
+      document.getElementById('audio22').play();
+      document.getElementById("w").style.backgroundColor = "cadetblue";
+      document.getElementById("w").style.color = "#fff";
+    }
+  });document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 88) {
+      document.getElementById('audio23').play();
+      document.getElementById("x").style.backgroundColor = "cadetblue";
+      document.getElementById("x").style.color = "#fff";
+    }
+  });document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 89) {
+      document.getElementById('audio24').play();
+      document.getElementById("y").style.backgroundColor = "cadetblue";
+      document.getElementById("y").style.color = "#fff";
+    }
+  });document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 90) {
+      document.getElementById('audio25').play();
+      document.getElementById("z").style.backgroundColor = "cadetblue";
+      document.getElementById("z").style.color = "#fff";
+    }
+  });
